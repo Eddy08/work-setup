3c054fd954d89c95f95dc1a8fc408c87f5a358d4
eddy08 <bhardwajharsh08@gmail.com>
2022-12-11 13:58:29 +0530

    Only Works with 3.9+ Version of Python ⛳

--- gen.py
+++ gen.py
@@ -84,7 +84,7 @@ def store_data(summary_location,summary_json,scan_dir):
         if not old_dict:
             updated_dict=new_dict
         else:
-           updated_dict=dict(new_dict.items() | old_dict.items())
+            updated_dict=new_dict | old_dict
 
         for k,v  in updated_dict.items():
             content_structure(writer=writer,file_path=k,file_model=v)

fd7eedc9c4a89cca73e611c125ef743a953ded9e
eddy08 <bhardwajharsh08@gmail.com>
2022-12-11 13:48:55 +0530

    Worked on Summary Logic ➕

--- gen.py
+++ gen.py
@@ -1,9 +1,18 @@
+import json
 import os
+import re
 import time
 import zipfile
+
+
+# Match Dirs which matches regex
+consider_dirs='Month?'
+
 # dict contains :
 file_dict={}
-store_location="./Task.txt"
+current_dir="./Work"
+summary_path="./Task.txt"
+summary_json="./Task.json"
 backup_location="Backup.zip"
 #List of Folders already inside the backup location
 backup_folders=[]
@@ -58,31 +67,57 @@ def content_structure(writer,file_path,file_model):
 
 # Get File Dict Data and Store it in the Task Folder
 
-def store_data(location):
+def store_data(summary_location,summary_json,scan_dir):
 
-    with open(location,'a') as writer:
-        for k,v  in get_file_data().items():
+    with open(summary_location,'w') as writer:
+        updated_dict={}
+        new_dict=get_file_data(scan=scan_dir)
+        old_dict={}
+        if os.path.exists(summary_json):
+            with open(summary_json,'r') as reader:
+                try:
+                    old_dict=json.load(reader)
+                    print("===?>",old_dict)
+                except Exception as e:
+                    print("Error Occured ",e )
+
+        if not old_dict:
+            updated_dict=new_dict
+        else:
+           updated_dict=dict(new_dict.items() | old_dict.items())
+
+        for k,v  in updated_dict.items():
             content_structure(writer=writer,file_path=k,file_model=v)
+    with open(summary_json,'w') as writer:
+        writer.write(json.dumps(updated_dict))
+
 
 
 
 # Get File Names in the Current Directory
-def get_file_data():
-    for obj in os.scandir():
-        if obj.is_file():
-            with open(obj.path,'r') as current_file_data:
-                file_model={}
-                
-                file_model['last_modification']=time.ctime(os.path.getmtime(obj.path))
-                file_model['content']=current_file_data.readlines();
-                file_dict[obj.path]=file_model
-                print("File Dictionary Details --> ",file_dict)
-                backup_file(file_path=obj.path)
-                delete_file(file_path=obj.path)    
-            # print(obj.path)
+def get_file_data(scan):
+    print("Scaning dir ==> ",scan)
+    for obj in os.scandir(scan):
+        print("objects in scandir() ==> ",obj)
+        if obj.is_dir() & bool(re.match(consider_dirs,obj.name)):
+            print("dir objects in scandir() ==> ",obj);
+            
+            for files in os.scandir(obj):
+
+                if files.is_file():
+                    with open(files.path,'r') as current_file_data:
+                        file_model={}
+                        
+                        file_model['last_modification']=time.ctime(os.path.getmtime(files.path))
+                        file_model['content']=current_file_data.readlines();
+                        file_dict[files.path]=file_model
+                        print("File Dictionary Details --> ",file_dict)
+                        # backup_file(file_path=files.path)
+                        # delete_file(file_path=files.path)    
+                    # print(files.path)
     print(file_dict)
     return file_dict;
 
 
 # Main 
-store_data(location=store_location)
+store_data(summary_location=summary_path,summary_json=summary_json,scan_dir=current_dir)
